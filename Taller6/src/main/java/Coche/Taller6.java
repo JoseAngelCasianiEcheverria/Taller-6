62d9a0ad87fa78dc3335616f20fd6e96ea53c862
JoseAngelCasianiEcheverria <joseangelcasianiecheverria@gmail.com>
2025-02-20 22:12:40 -0500

    Ejercicio 1 Taller 6

---
+++ Taller6/src/main/java/Coche/Taller6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Coche;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Taller6 {
+
+    public static void main(String[] args) {
+        Coche coche1 = new Coche("Toyota", "Corolla");
+        Coche coche2 = new Coche("Honda", "Civic");
+        Coche coche3 = new Coche("Ford", "Focus");
+
+        Coche.CochesCreados();
+    }
+}
