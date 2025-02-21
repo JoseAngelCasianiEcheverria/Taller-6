ed5733b08b0e3b44ab575ba9912f6e4da2cad356
JoseAngelCasianiEcheverria <joseangelcasianiecheverria@gmail.com>
2025-02-20 22:13:35 -0500

    Ejercicio 3 Taller 6

---
+++ Ejemplo_clasePOO/src/main/java/uml/Ejemplo_clasePOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uml;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Ejemplo_clasePOO {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Ejemplo_clasePOO/src/main/java/uml/Prestamista.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uml;
+
+/**
+ *
+ * @author Gercray
+ */
+public class Prestamista {
+    private String identificacion;
+    private String tipoIdentificacion;
+    private String primerNombre;
+    private String segundoNombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private String email;
+    private String numeroTelefono;
+    private String direccion;
+    
+    //constructores
+    public Prestamista(){
+        
+    }
+    
+    public Prestamista(String id, String tipoId, String pNombre, String pApellido){
+        identificacion = id;
+        tipoIdentificacion = tipoId;
+        primerNombre = pNombre;
+        primerApellido = pApellido;
+    }
+    //get y set
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getTipoIdentificacion() {
+        return tipoIdentificacion;
+    }
+
+    public void setTipoIdentificacion(String tipoIdentificacion) {
+        this.tipoIdentificacion = tipoIdentificacion;
+    }
+
+    public String getPrimerNombre() {
+        return primerNombre;
+    }
+
+    public void setPrimerNombre(String primerNombre) {
+        this.primerNombre = primerNombre;
+    }
+
+    public String getSegundoNombre() {
+        return segundoNombre;
+    }
+
+    public void setSegundoNombre(String segundoNombre) {
+        this.segundoNombre = segundoNombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+}
